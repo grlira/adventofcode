bf2fd9337d1a9ab2e150e5efe5d0a426fa68eccc
Gustavo Lira <gustavo@grlira.com>
2019-12-01 22:21:13 +0100

    Day 1 - part 2

--- day1/day1.js
+++ day1/day1.js
@@ -1,10 +1,21 @@
 const fs = require("fs");
 
-const lines = fs.readFileSync("input.txt");
+if (!process.argv[2]) {
+  console.error("missing input file name");
+  process.exit(1);
+}
+
+const lines = fs.readFileSync(process.argv[2]);
+
+function fuelCost(mass) {
+  const fuel = Math.max(Math.floor(mass / 3) - 2, 0);
+  return fuel > 0 ? fuel + fuelCost(fuel) : fuel;
+}
+
 console.log(
   lines
     .toString()
     .split("\n")
-    .map(line => Math.floor(parseInt(line, 10) / 3) - 2)
+    .map(line => fuelCost(parseInt(line, 10)))
     .reduce((total, fuel) => total + fuel, 0)
 );
