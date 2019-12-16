7e423a446172c01878a0c94fb53962570b1700c7
Gustavo Ramos Lira (GUSL) <gusl@saxobank.com>
2019-12-16 08:38:52 +0100

    Day 13 - part 2

--- day13/day13.js
+++ day13/day13.js
@@ -1,6 +1,12 @@
 const fs = require("fs");
 const VM = require("./vm").default;
 
+const readline = require("readline");
+
+const RIGHT = "\u001b[C";
+const LEFT = "\u001b[D";
+const UP = "\u001b[A";
+
 if (!process.argv[2]) {
   console.error("missing input file name");
   process.exit(1);
@@ -15,14 +21,14 @@ function getTile(tileCode) {
     case 2:
       return "Q";
     case 3:
-      return "_";
+      return "T";
     case 4:
       return "o";
   }
 }
 
 function printScreen(screen) {
-  screen.forEach(line => console.log(line.join("")));
+  console.log(screen.map(line => line.join("")).join("\n"));
 }
 
 function flatten(array) {
@@ -37,31 +43,94 @@ const program = fs
 
 program[0] = 2; // number of quarters inserted
 
-const myvm = VM([...program]);
-myvm.run();
+function interactive() {
+  const myvm = VM([...program]);
 
-const screen = [];
-let score = 0;
-while (myvm.outputs.length !== 0) {
-  const x = myvm.outputs.shift();
-  const y = myvm.outputs.shift();
-  const tile = myvm.outputs.shift();
+  const screen = [];
+  let score = 0;
+  readline.emitKeypressEvents(process.stdin);
+  process.stdin.setRawMode(true);
 
-  if (x === -1 && y === 0) {
-    score = tile;
-    continue;
-  }
+  process.stdin.on("keypress", (key, data) => {
+    if (data.sequence === UP) {
+      process.exit();
+    }
+    if (data.sequence === RIGHT) {
+      myvm.inputs.push(1);
+    } else if (data.sequence === LEFT) {
+      myvm.inputs.push(-1);
+    } else {
+      myvm.inputs.push(0);
+    }
+    if (myvm.run()) {
+      process.exit();
+    }
+    while (myvm.outputs.length !== 0) {
+      const x = myvm.outputs.shift();
+      const y = myvm.outputs.shift();
+      const tile = myvm.outputs.shift();
 
-  if (!screen[y]) {
-    screen[y] = [];
-  }
-  screen[y][x] = getTile(tile);
+      if (x === -1 && y === 0) {
+        score = tile;
+        continue;
+      }
+
+      if (!screen[y]) {
+        screen[y] = [];
+      }
+      screen[y][x] = getTile(tile);
+    }
+    console.clear();
+    printScreen(screen);
+    console.log(`Score: ${score}`);
+  });
 }
-printScreen(screen);
 
-const totalBlocks = flatten(screen).reduce(
-  (total, tile) => (tile === "Q" ? total + 1 : total),
-  0
-);
+function automated() {
+  const paddle = { x: 0, y: 0 };
+  const ball = { x: 0, y: 0 };
+  const myvm = VM([...program]);
 
-console.log(`Part 1: ${totalBlocks}`);
+  const screen = [];
+  let score = 0;
+
+  while (!myvm.run()) {
+    while (myvm.outputs.length !== 0) {
+      const x = myvm.outputs.shift();
+      const y = myvm.outputs.shift();
+      const tile = myvm.outputs.shift();
+
+      if (x === -1 && y === 0) {
+        score = tile;
+        continue;
+      }
+
+      if (!screen[y]) {
+        screen[y] = [];
+      }
+      screen[y][x] = getTile(tile);
+      if (tile === 3) {
+        paddle.x = x;
+        paddle.y = y;
+      }
+      if (tile === 4) {
+        ball.x = x;
+        ball.y = y;
+      }
+    }
+    console.clear();
+    printScreen(screen);
+    console.log(`Score: ${score}`);
+    if (paddle.x < ball.x) {
+      myvm.inputs.push(1);
+    } else if (paddle.x > ball.x) {
+      myvm.inputs.push(-1);
+    } else {
+      myvm.inputs.push(0);
+    }
+  }
+  console.log(`Final score: ${score}`);
+  console.log(`Final output: ${myvm.outputs}`);
+}
+
+automated();
